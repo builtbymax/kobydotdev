51298263bff3e467fb013dab3bb5c64782831e1a
Maximilian <code@koby.dev>
2023-09-13 08:53:09 +0200

    [TASK] Added favicon (placeholder) && fixed translation error for projects

--- dictionary.js
+++ dictionary.js
@@ -3,9 +3,9 @@ import 'server-only';
 const dictionaries = {
   en: () => import('./dictionaries/en.json').then((module) => module.default),
   de: () => import('./dictionaries/de.json').then((module) => module.default),
-};
+}
 
 export const getDictionary = async (locale) => {
   const dictionary = await dictionaries[locale]();
-  return dictionary;
-};
+  return dictionary
+}
